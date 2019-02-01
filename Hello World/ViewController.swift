6bd59f921fbddbd8db74d52780b8d14397e64481
Mitchell Hoffmann <mitchellhoffmann1@gmail.com>
2019-01-31 19:44:29 -0600

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Mitchell John Hoffmann on 1/31/19.
+//  Copyright © 2019 Mitchell John Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
